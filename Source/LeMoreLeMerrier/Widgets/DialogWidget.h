2ea899bc813c36fc6e5a582d9c55f914099dc631
cluert <ryan.clue@gmail.com>
2023-11-08 23:00:06 -0800

    Improved UI Dialog visuals, talkable villagers, enabled rvo avoidance for ai

---
+++ Source/LeMoreLeMerrier/Widgets/DialogWidget.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DialogWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEMORELEMERRIER_API UDialogWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Meta = (ExposeOnSpawn = true), Category="Dialog")
+	class UDialogComponent* DialogReader;
+
+};
